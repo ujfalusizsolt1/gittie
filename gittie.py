c30af90cee6e6e7a34b1ac1dff5797ec5a883c7b
Peter Kovacs <kovacs.g.peter@gmail.com>
2017-11-22 09:30:59 +0100

    introduce changed Peter

--- gittie.py
+++ gittie.py
@@ -4,9 +4,7 @@ def add(a, b):
 
 def introduce():
     print("Hello, I'm Gittie!")
-<<<<<<< HEAD
-=======
+    print("Bela")
 
 def joke():
     print("a joke")
->>>>>>> f2052aed7b84ebcc9768862c5ee5368f40d68f97
