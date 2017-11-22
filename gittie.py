acc2de75356d84bbaf6cd49e668d7cf0136fbfd9
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2017-11-22 09:45:51 +0100

    correct

--- gittie.py
+++ gittie.py
@@ -3,13 +3,9 @@ def add(a, b):
 
 
 def introduce():
-<<<<<<< HEAD
     print("Hello, I'm Carrot!")
-=======
     print("Hello, I'm bela")
->>>>>>> 8c231dbdc7ee23c1c84e9480c088abeffd581abd
 
 
 def joke():
     print("a joke")
-
